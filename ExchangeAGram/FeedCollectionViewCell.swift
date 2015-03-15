25440f68ce81143692302b38f7faf4b430a91b07
iamtonyarmstrong <i.am@iamtonyarmstrong.com>
2015-03-15 14:08:54 -0400

    Added custom cell code

---
+++ ExchangeAGram/FeedCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCollectionViewCell.swift
+//  ExchangeAGram
+//
+//  Created by Anthony Armstrong on 3/14/15.
+//  Copyright (c) 2015 openinformant. All rights reserved.
+//
+
+import UIKit
+
+class FeedCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellLabel: UILabel!
+}
