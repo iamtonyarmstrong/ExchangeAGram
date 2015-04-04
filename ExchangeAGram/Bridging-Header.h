456f94f62f0ec36f976177c9a2dee2e235add362
iamtonyarmstrong <i.am@iamtonyarmstrong.com>
2015-04-04 16:38:01 -0400

    Added Facebook SDK

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Anthony Armstrong on 4/4/15.
+//  Copyright (c) 2015 openinformant. All rights reserved.
+//
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
